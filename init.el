d076c484b1848091bb31ea5b0bc51cf4ec62b79f
zyl-hub <54789212+zyl-hub@users.noreply.github.com>
2024-04-26 14:35:46 +0800

    use straight.el to install package and set org startup indent

---
+++ init.el
@@ -0,0 +1,23 @@
+(setq lexical-binding t)
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+(setq custom-file (locate-user-emacs-file "custom.el"))
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(require 'init-org)
