8834b4ac8da39c970728b19e19553efcf04075fa
yl <1016076582@qq.com>
2024-05-17 21:58:10 +0800

    fix cdlatex in org-mode

--- lisp/init-latex.el
+++ lisp/init-latex.el
@@ -6,7 +6,8 @@
 (setup cdlatex
   (:straight cdlatex)
   (:with-function turn-on-cdlatex
-    (:hook-into LaTeX-mode-hook latex-mode-hook org-mode-hook))
-  (:bind "<tab>" cdlatex-tab))
+    (:hook-into LaTeX-mode-hook latex-mode-hook))
+  (:with-function turn-on-org-cdlatex
+    (:hook-into org-mode-hook)))
 
 (provide 'init-latex)
