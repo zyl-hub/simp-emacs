64ace344b0d594f79916c9ae6d15cd3d99954977
yl <1016076582@qq.com>
2025-03-24 15:18:05 +0800

    fix DeepSeek reasoning

--- lisp/init-gptel.el
+++ lisp/init-gptel.el
@@ -3,12 +3,12 @@
 (setup gptel
   (:straight gptel)
   (setq gptel-backend
-	(gptel-make-openai "test"      ;Any name you want
-			   :host "aihubmix.com"
-			   :endpoint "/v1/chat/completions"
-			   :stream t
-			   :key "sk-DQiMnpBL8xZTqiqM6f8c937f4c774e1c98B42d164bBe2466"
-			   :models '(DeepSeek-R1 ;后面可以补充任意的模型名
-				     ))))
+	(gptel-make-deepseek "DeepSeek"      ;Any name you want
+	  :host "aihubmix.com"
+	  :endpoint "/v1/chat/completions"
+	  :stream t
+	  :key "sk-DQiMnpBL8xZTqiqM6f8c937f4c774e1c98B42d164bBe2466"
+	  :models '(DeepSeek-R1 ;后面可以补充任意的模型名
+		    ))))
 
 (provide 'init-gptel)
