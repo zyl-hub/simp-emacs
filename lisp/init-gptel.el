71ea15de5946342ca079751fbc3ab1bdc8d86627
yl <1016076582@qq.com>
2025-03-25 20:04:18 +0800

    set deepseek distill qwen 32B model as default

--- lisp/init-gptel.el
+++ lisp/init-gptel.el
@@ -2,13 +2,16 @@
 
 (setup gptel
   (:straight gptel)
+  (setq gptel-default-mode 'org-mode)
   (setq gptel-backend
-	(gptel-make-deepseek "DeepSeek"      ;Any name you want
+	(gptel-make-deepseek "DeepSeek-R1"      ;Any name you want
 	  :host "aihubmix.com"
 	  :endpoint "/v1/chat/completions"
 	  :stream t
 	  :key "sk-DQiMnpBL8xZTqiqM6f8c937f4c774e1c98B42d164bBe2466"
-	  :models '(DeepSeek-R1 ;后面可以补充任意的模型名
-		    ))))
+	  :models '(deepseek-ai/DeepSeek-R1-Distill-Qwen-32B ;后面可以补充任意的模型名
+		    )))
+  (setq gptel-model 'deepseek-ai/DeepSeek-R1-Distill-Qwen-32B)
+  )
 
 (provide 'init-gptel)
