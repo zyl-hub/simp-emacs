129d19f46d3ecf69aee50200e77e9798fb738270
yl <1016076582@qq.com>
2025-04-08 14:17:33 +0800

    add theme, add telega

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -4,12 +4,15 @@
   (set-face-attribute
    'default nil
    :font (font-spec :family "CMU Typewriter Text"
-   ;; :font (font-spec :family "MonaspiceNe NF"
+		    ;; :font (font-spec :family "MonaspiceNe NF"
                     :weight 'normal
                     :slant 'normal
                     :size (cond ((eq system-type 'gnu/linux) 14.0)
                                 ((eq system-type 'windows-nt) 12.0))))
-  
+
+  (set-fontset-font t 'unicode
+		    (font-spec :family "Noto Color Emoji"))
+
   (set-fontset-font t '(#x4e00 . #x9fff)
 		    ;; (font-spec :family "LXGW Wenkai"
 		    (font-spec :family (cond ((eq system-type 'gnu/linux) "Noto Sans CJK SC")
@@ -18,7 +21,11 @@
 			       :slant 'normal
 			       :size (cond ((eq system-type 'gnu/linux) 14.0)
 					   ((eq system-type 'windows-nt) 12.0))))
-    
+
+  ;; (set-fontset-font t 'symbol
+  ;; (font-spec :family "Noto Color Emoji"))
+
+  
   (set-face-attribute
    'italic nil
    :font (font-spec :family "CMU Classical Serif"
@@ -31,15 +38,17 @@
   (:global [f5] (lambda() (interactive) (find-file user-init-file)))
   (set-language-environment "utf-8")
   ;; (when (memq system-type '(cygwin windows-nt ms-dos))
-    ;; (setq default-process-coding-system '(gbk . gbk)))
+  ;; (setq default-process-coding-system '(gbk . gbk)))
   ;; (add-to-list 'process-coding-system-alist
-	       ;; '("cmdproxy" utf-8 . gbk))
+  ;; '("cmdproxy" utf-8 . gbk))
   ;; (fido-vertical-mode)
   (setq default-directory (cond ((eq system-type 'windows-nt) "c:/project/")
-			      ((eq system-type 'gnu/linux) "~/")))
+				((eq system-type 'gnu/linux) "~/")))
   (save-place-mode 1)
   ;; (desktop-save-mode 1)
   (setq make-backup-files nil)
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
   (setq inhibit-startup-screen t))
 
 (setup font
@@ -57,6 +66,22 @@
   (:global "M-g w" #'avy-goto-word-1)
   (:global "M-g c" #'avy-goto-char-2)
   (:global "M-g l" #'avy-goto-line))
+
+(setup doom-themes
+  (:straight doom-themes)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-one t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (nerd-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
 
 (provide 'init-basic)
 
--- lisp/init-telega.el
+++ lisp/init-telega.el
@@ -1,6 +1,16 @@
 ;;-*- lexical-binding: t -*-
 
 (setup telega
-  (:straight telega))
+  (:straight all-the-icons)
+  (:straight telega)
+  (add-hook 'telega-load-hook
+            (lambda ()
+              (define-key global-map (kbd "C-c t") telega-prefix-map)))
+  (setq telega-avatar-workaround-gaps-for '(return t))
+  (setq telega-emoji-font-family (font-spec :family "Noto Color Emoji")
+	telega-emoji-use-images nil)
+  )
+
+
 
 (provide 'init-telega)
